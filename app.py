0064cced03f1b63228431cf064618c3d3eda8427
EustaceMwai <43333927+EustaceMwai@users.noreply.github.com>
2021-02-09 21:48:53 +0300

    adding links

--- app.py
+++ app.py
@@ -40,36 +40,55 @@ def index():
     cur = db.execute('select entry_date from log_date order by entry_date desc')
     results = cur.fetchall()
 
-    pretty_results = []
+    date_results = []
 
     for i in results:
         single_date = {}
 
+        single_date['entry_date'] = i['entry_date']
+
         d = datetime.strptime(str(i['entry_date']), '%Y%m%d')
-        single_date['entry_date'] = datetime.strftime(d, '%B %d, %Y')
+        single_date['pretty_date'] = datetime.strftime(d, '%B %d, %Y')
 
-        pretty_results.append(single_date)
+        date_results.append(single_date)
 
-    return render_template('home.html', results=pretty_results)
+    return render_template('home.html', results=date_results)
 
 
 @app.route('/view/<date>', methods=['GET', 'POST'])
 def view(date):
-    if request.method == 'POST':
-        return '<h1> The food item is #{} </h1>'.format(request.form['food-select'])
-
     db = get_db()
 
-    cur = db.execute('select entry_date from log_date where entry_date = ?', [date])
-    result = cur.fetchone()
+    cur = db.execute('select id, entry_date from log_date where entry_date = ?', [date])
+    date_result = cur.fetchone()
 
-    d = datetime.strptime(str(result['entry_date']), '%Y%m%d')
+    if request.method == 'POST':
+        db.execute('insert into food_date (food_id, log_date_id) values (?, ?)', [request.form['food-select'], date_result['id']])
+        db.commit()
+
+
+    d = datetime.strptime(str(date_result['entry_date']), '%Y%m%d')
     pretty_date = datetime.strftime(d, '%B %d, %Y')
 
     food_cur = db.execute('select id, name from food')
     food_results = food_cur.fetchall()
 
-    return render_template('day.html', date=pretty_date, food_results=food_results)
+    log_cur = db.execute('select food.name, food.protein, food.carbohydrates, food.fat, food.calories from log_date join food_date on food_date.log_date_id = log_date.id join food on food.id = food_date.food_id where log_date.entry_date = ?', [date])
+    log_results = log_cur.fetchall()
+
+    totals = {}
+    totals['protein'] = 0
+    totals['carbohydrates'] = 0
+    totals['fat'] = 0
+    totals['calories'] = 0
+
+    for food in  log_results:
+        totals['protein'] += food['protein']
+        totals['carbohydrates'] += food['carbohydrates']
+        totals['fat'] += food['fat']
+        totals['calories'] += food['calories']
+
+    return render_template('day.html', entry_date=date_result['entry_date'], pretty_date=pretty_date, food_results=food_results, log_results=log_results, totals=totals)
 
 
 @app.route('/food', methods=['GET', 'POST'])
