11f94c1c072d91f58e9fc90a4d026dc6ddc26a44
EustaceMwai <43333927+EustaceMwai@users.noreply.github.com>
2021-02-10 23:06:24 +0300

    finishing on links and db

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 from Tools.scripts.make_ctype import method
 from flask import Flask, render_template, g, request
-import sqlite3
+
 from datetime import datetime
+from database import connect_db, get_db
 
 app = Flask(__name__)
-
-
-def connect_db():
-    sql = sqlite3.connect('food_log.db')
-    sql.row_factory = sqlite3.Row
-    return sql
-
-
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
 
 
 @app.teardown_appcontext
@@ -37,7 +26,10 @@ def index():
         db.execute('insert into log_date (entry_date) values (?)', [database_date])
         db.commit()
 
-    cur = db.execute('select entry_date from log_date order by entry_date desc')
+    cur = db.execute('''select log_date.entry_date, sum(food.protein) as protein, sum(food.carbohydrates) as carbohydrates, sum(food.fat) as fat, sum(food.calories) as calories
+                        from log_date 
+                        left join food_date on food_date.log_date_id = log_date.id 
+                        left  join food on food.id = food_date.food_id group by log_date.id order by log_date.entry_date desc''')
     results = cur.fetchall()
 
     date_results = []
@@ -46,6 +38,10 @@ def index():
         single_date = {}
 
         single_date['entry_date'] = i['entry_date']
+        single_date['protein'] = i['protein']
+        single_date['carbohydrates'] = i['carbohydrates']
+        single_date['fat'] = i['fat']
+        single_date['calories'] = i['calories']
 
         d = datetime.strptime(str(i['entry_date']), '%Y%m%d')
         single_date['pretty_date'] = datetime.strftime(d, '%B %d, %Y')
@@ -63,9 +59,9 @@ def view(date):
     date_result = cur.fetchone()
 
     if request.method == 'POST':
-        db.execute('insert into food_date (food_id, log_date_id) values (?, ?)', [request.form['food-select'], date_result['id']])
+        db.execute('insert into food_date (food_id, log_date_id) values (?, ?)',
+                   [request.form['food-select'], date_result['id']])
         db.commit()
-
 
     d = datetime.strptime(str(date_result['entry_date']), '%Y%m%d')
     pretty_date = datetime.strftime(d, '%B %d, %Y')
@@ -73,7 +69,11 @@ def view(date):
     food_cur = db.execute('select id, name from food')
     food_results = food_cur.fetchall()
 
-    log_cur = db.execute('select food.name, food.protein, food.carbohydrates, food.fat, food.calories from log_date join food_date on food_date.log_date_id = log_date.id join food on food.id = food_date.food_id where log_date.entry_date = ?', [date])
+    log_cur = db.execute('''select food.name, food.protein, food.carbohydrates, food.fat, food.calories 
+                        from log_date 
+                        join food_date on food_date.log_date_id = log_date.id 
+                        join food on food.id = food_date.food_id 
+                        where log_date.entry_date = ?''', [date])
     log_results = log_cur.fetchall()
 
     totals = {}
@@ -82,13 +82,14 @@ def view(date):
     totals['fat'] = 0
     totals['calories'] = 0
 
-    for food in  log_results:
+    for food in log_results:
         totals['protein'] += food['protein']
         totals['carbohydrates'] += food['carbohydrates']
         totals['fat'] += food['fat']
         totals['calories'] += food['calories']
 
-    return render_template('day.html', entry_date=date_result['entry_date'], pretty_date=pretty_date, food_results=food_results, log_results=log_results, totals=totals)
+    return render_template('day.html', entry_date=date_result['entry_date'], pretty_date=pretty_date,
+                           food_results=food_results, log_results=log_results, totals=totals)
 
 
 @app.route('/food', methods=['GET', 'POST'])
